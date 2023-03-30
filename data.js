5b3251589ac5f1fc16bc136cece99f4fed4e7ec7
GenghisCahn <115155331+GenghisCahn@users.noreply.github.com>
2023-03-30 22:35:23 +0100

    added blog page
    commit

--- data.js
+++ data.js
@@ -3,6 +3,10 @@ export const blogPosts = [
     uuid: 'a3db8b68-c9b2-4a05-9d25-8b1cb6b742a6',
     title: 'Why JavaScript is a great language to learn',
     paragraph: 'JavaScript is one of the most popular programming languages in the world, and for good reason. Its versatility and ease of use make it a great language to learn for both beginners and advanced programmers alike.',
+    subHeadingOne: "5 Common Mistakes to Avoid When Writing JavaScript Code",
+    subHeadingTwo: "10 Essential JavaScript Libraries and Frameworks to Boost Your Development",
+    bodyOne: 'JavaScript (JS) is a popular programming language used for creating dynamic and interactive web applications. It is often referred to as the language of the web, as it runs on almost all modern web browsers and can be used for both front-end and back-end development.',
+    bodyTwo: 'JavaScript was first introduced in 1995 by Netscape as a scripting language for their web browser. Over the years, it has evolved into a full-fledged programming language, with support for object-oriented programming, functional programming, and asynchronous programming.',
     image: 'images/js-img.jpg',
     date: '2022-05-15'
   },
@@ -10,6 +14,10 @@ export const blogPosts = [
     uuid: 'c2a4990a-72b2-4211-8c9e-c03ecf0e0f3b',
     title: '10 Tips for Better Productivity in the Workplace',
     paragraph: 'Productivity is key to success in the workplace. Here are 10 tips to help you be more productive and achieve your goals.',
+    subHeadingOne: "The Power of Prioritization: How to Get More Done in Less Time",
+    subHeadingTwo: "Maximizing Your Productivity: Tips and Techniques for Working from Home",
+    bodyOne: 'Productivity is the measure of how effectively we use our time, energy, and resources to achieve our goals. In todays fast-paced world, productivity has become increasingly important individuals, teams, and organizations looking to stay competitive and achieve their objectives.',
+    bodyTwo: 'There are several strategies and techniques that can be used to improve productivity, including time management, goal setting, prioritization, focus, and delegation. By implementing these strategies, individuals can become more productive and accomplish more in less time.',
     image: 'images/blog2.jpg',
     date: '2022-06-01'
   },
@@ -17,6 +25,10 @@ export const blogPosts = [
     uuid: '91ab3d37-9b14-4f0c-90d6-fd0e37b39da1',
     title: 'The Benefits of Meditation for Mental Health',
     paragraph: 'Meditation has been shown to have numerous benefits for mental health, including reducing stress and anxiety, improving focus and attention, and increasing feelings of well-being. Learn more about the benefits of meditation and how to get started.',
+    subHeadingOne: "The Science of Mindfulness: How Meditation Can Benefit Your Mental and Physical Health",
+    subHeadingTwo: "Finding Inner Peace: A Beginner's Guide to Meditation Techniques and Practices",
+    bodyOne: 'Meditation is a practice that involves training the mind to focus on the present moment, with the goal of achieving a calm and relaxed state of mind. It has been practiced for thousands of years and is a key component of many spiritual and religious traditions.',
+    bodyTwo: 'However, in recent years, meditation has gained popularity as a secular practice with numerous health benefits.',
     image: 'images/blog3.jpg',
     date: '2022-07-12'
   },
@@ -24,6 +36,10 @@ export const blogPosts = [
     uuid: '1e7c36a5-974a-4be6-9869-2c8d76ebc95d',
     title: 'How to Build a Successful E-Commerce Business',
     paragraph: 'E-commerce is a rapidly growing industry, and starting an online store can be a great way to earn money and build a successful business. Here are some tips for building a successful e-commerce business.',
+    subHeadingOne: "The Future of E-Commerce: Trends and Innovations to Watch Out For",
+    subHeadingTwo: "Mastering the Art of Online Sales: Strategies for Growing Your E-Commerce Business",
+    bodyOne: 'One of the biggest advantages of e-commerce is convenience. With e-commerce, consumers can shop online from the comfort of their homes, without having to physically visit a store.',
+    bodyTwo: 'This can save time and money on travel, parking, and other expenses associated with traditional shopping. In addition, e-commerce allows consumers to shop 24/7, without having to worry about store hours or closures.',
     image: 'images/blog6.jpg',
     date: '2022-08-05'
   },
@@ -31,6 +47,10 @@ export const blogPosts = [
     uuid: 'c12eaf77-828f-4172-8d36-056c74f68a0e',
     title: 'The Future of Artificial Intelligence in Healthcare',
     paragraph: 'Artificial intelligence (AI) is transforming the healthcare industry, with applications ranging from predicting diseases to improving patient outcomes. Learn more about the future of AI in healthcare and how it is changing the way we approach medical care.',
+    subHeadingOne: "Exploring the Ethics of Artificial Intelligence: Balancing Innovation and Responsibility",
+    subHeadingTwo: "AI for Good: How Artificial Intelligence is Being Used to Solve Global Challenges",
+    bodyOne: 'Artificial intelligence, or AI, is a rapidly evolving technology that is changing the way we live and work. AI refers to the development of computer systems that can perform tasks that typically require human intelligence, such as visual perception, speech recognition, and decision-making.',
+    bodyTwo: 'One of the main benefits of AI is its ability to automate tasks and increase efficiency. By using AI to perform repetitive or tedious tasks, businesses can free up their employees to focus on higher-level tasks that require human creativity and critical thinking. This can lead to increased productivity and reduced costs.',
     image: 'images/ai-img.jpg',
     date: '2022-09-23'
   },
@@ -38,6 +58,10 @@ export const blogPosts = [
     uuid: '7df064bc-9a1a-45ed-9b7c-23d44634b496',
     title: 'The Benefits of Yoga for Physical and Mental Health',
     paragraph: 'Yoga has been shown to have numerous benefits for both physical and mental health, including improving flexibility and strength, reducing stress and anxiety, and promoting relaxation. Learn more about the benefits of yoga and how to get started with a regular practice.',
+    subHeadingOne: "Breaking the Stigma: Addressing Mental Health in the Workplace",
+    subHeadingTwo: "Self-Care for Better Mental Health: Tips and Techniques for Prioritizing Your Well-Being",
+    bodyOne: 'Yoga is a practice that has been around for thousands of years and is known for its physical and mental health benefits. The practice of yoga combines physical postures, breathing techniques, and meditation, all of which can have a positive impact on mental health.',
+    bodyTwo: 'Studies have shown that regular yoga practice can help to reduce stress, anxiety, and depression. This is because yoga postures and breathing techniques help to activate the parasympathetic nervous system, which promotes relaxation and reduces stress. Yoga also helps to improve mood by increasing the production of serotonin and dopamine, two neurotransmitters that are associated with feelings of happiness and well-being.',
     image: 'images/blog4.jpg',
     date: '2022-10-10'
   }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import {blogPosts} from './data.js'
 
 const blogContainer = document.getElementById('blog-container')
+const blogPageContainer = document.getElementById('blog-page-container')
 
 function renderBlogPosts() {
     let blogHtml = ""
@@ -15,7 +16,34 @@ function renderBlogPosts() {
         </div>`
     }
     blogContainer.innerHTML = blogHtml
+    
+    const blogPage = document.getElementsByClassName('blog')
+    
+    for (let i = 0; i < blogPage.length; i++){
+        blogPage[i].addEventListener('click', function (){
+            renderBlogPage(blogPosts[i])
+        })
+    }   
 }
 
 renderBlogPosts()
 
+function renderBlogPage(blog) {
+    document.getElementById('hero-img').classList.add('hidden')
+    document.getElementById('hero-text').classList.add('hidden')
+    
+   blogPageContainer.innerHTML = `
+    <div class="blog-header">
+        <h1>${blog.title}</h1>
+        <p>${blog.paragraph}</p>
+    </div>
+    <img class="blog-page-img" src="${blog.image}">
+    <div class="blog-body">
+        <h3>${blog.subHeadingOne}</h3>
+        <p>${blog.bodyOne}</p>
+    </div>
+    <div class="blog-body">
+        <h3>${blog.subHeadingTwo}</h3>
+        <p>${blog.bodyTwo}</p>
+    </div>`
+}
